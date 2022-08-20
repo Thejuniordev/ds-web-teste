a9823cd906696b46fee821d361864810b75377eb
Junior Alves <junioralves.webdeveloper@gmail.com>
2022-08-20 18:01:53 -0300

    fix: Add animate button

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -5,6 +5,9 @@ export const Main = () => {
 
   return (
     <div className="main">
+      <Button color={"default"} disabled={false} size="small">Send</Button>
+      <Button color={"default"} disabled={false} size="medium">Send</Button>
+      <Button color={"default"} disabled={true} size="small">Send</Button>
     </div>
   );
 }
