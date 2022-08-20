ada080394077af981a77fe7eac942406fab8699b
Junior Alves <junioralves.webdeveloper@gmail.com>
2022-08-20 20:11:48 -0300

    fix: Main custom page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import Button from "../components/DS/Button";
 import InputText from "../components/DS/Input";
 
+import Logo from '../assets/img/logo.png';
+
+import './main.scss';
+
 export const Main = () => {
 
   return (
     <div className="main">
+      <div className="container">
+        <figure className="main__logo">
+          <img src={Logo} alt="Carrefour soluções financeiras" />
+        </figure>
+
+        <h1 className="main__title">Informe seus dados</h1>
+      </div>
     </div>
   );
 }
